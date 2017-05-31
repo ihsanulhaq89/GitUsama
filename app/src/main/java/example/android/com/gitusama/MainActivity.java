aedb6edb728401eeff0ec52bdacc1a27d509a32b
ihsanulhaq <ihsanulhaq89@gmail.com>
2017-05-31 15:11:46 +0500

    init

---
+++ app/src/main/java/example/android/com/gitusama/MainActivity.java
@@ -0,0 +1,13 @@
+package example.android.com.gitusama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
